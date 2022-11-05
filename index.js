d1b959006a45f9c096952a3d7c92e06ca31023c5
Blake Hamilton <blha2078@colorado.edu>
2022-11-04 18:15:45 -0600

    search_songs route is functional.
    sends user back with title, artist name, pitcure link, and key value for songs

--- index.js
+++ index.js
@@ -1,228 +1,208 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const pgp = require("pg-promise")();
-const bodyParser = require("body-parser");
-const session = require("express-session");
-const bcrypt = require("bcrypt");
-const axios = require("axios");
-const { URLSearchParams } = require("url");
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const axios = require('axios');
+const { URLSearchParams } = require('url');
+const { resolveNaptr } = require('dns');
 
 // database configuration
 const dbConfig = {
-  host: "db",
-  port: 5432,
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+    host: 'db',
+    port: 5432,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
 };
-
+  
 const db = pgp(dbConfig);
 
 // test your database
 db.connect()
-  .then((obj) => {
-    console.log("Database connection successful"); // you can view this message in the docker compose logs
+.then(obj => {
+    console.log('Database connection successful'); // you can view this message in the docker compose logs
     obj.done(); // success, release the connection;
-  })
-  .catch((error) => {
-    console.log("ERROR:", error.message || error);
-  });
+})
+.catch(error => {
+    console.log('ERROR:', error.message || error);
+});
 
-app.set("view engine", "ejs");
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
 
 app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
-    resave: false,
-  })
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+    })
+);
+    
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
 );
 
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
 
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
-var access_token = "";
-var refresh_token = "";
+var access_token = '';
+var refresh_token = '';
 
-app.get("/", (req, res) => {
-  console.log("/ route");
-  // res.send('home');
-  res.render("pages/home");
+
+app.get('/', (req, res) =>{
+    console.log('/ route');
+    res.send('home');
 });
+
 
 // Necessary callback route for SPotify API authentication
 // SHould not be called directly by client
-app.get("/callback", async (req, res) => {
-  console.log("/callback route");
+app.get('/callback', async (req, res) => {
+    console.log('/callback route');
 
-  code = req.query.code;
-  console.log(req.query.code);
+    code = req.query.code;
+    console.log(req.query.code);
 
-  const authUrl = "https://accounts.spotify.com/api/token";
+    const authUrl = 'https://accounts.spotify.com/api/token'
 
-  axios({
-    url: authUrl,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      // From Spotify documentation
-      Authorization:
-        "Basic " +
-        new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
-    },
-    // data must be x-www-urlform-encoded, so it must be turned into a URL search param object
-    data: new URLSearchParams({
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: REDIRECT_URI,
-    }),
-  })
-    .then((results) => {
-      // Successful case
-      console.log(results.data);
-      access_token = results.data.access_token;
-      refresh_token = results.data.refresh_token;
-      res.send(results.data);
+    axios({
+        url: authUrl,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            // From Spotify documentation
+            'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+        },
+        // data must be x-www-urlform-encoded, so it must be turned into a URL search param object
+        data: new URLSearchParams( {
+            "grant_type":    "authorization_code",
+            "code":          code,
+            "redirect_uri":  REDIRECT_URI
+        }),
     })
-    .catch((err) => {
-      // Handle errors
-      console.log(err);
-      res.send("Error. Check console log");
+    .then(results => {
+        // Successful case
+        console.log(results.data);
+        access_token = results.data.access_token;
+        refresh_token = results.data.refresh_token;
+        res.send(results.data);
+    })
+    .catch(err => {
+        // Handle errors
+        console.log(err);
+        res.send('Error. Check console log');
     });
 });
 
+
 // Refresh token route for SPotify API authentication
-app.get("/refresh_token", async (req, res) => {
-  console.log("/refresh route");
+app.get('/refresh_token', async (req, res) => {
+    console.log('/refresh route');
 
-  console.log(refresh_token);
+    console.log(refresh_token);
 
-  const refreshUrl = "https://accounts.spotify.com/api/token";
+    const refreshUrl = 'https://accounts.spotify.com/api/token'
 
-  axios({
-    url: refreshUrl,
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-      // From Spotify documentation
-      Authorization:
-        "Basic " +
-        new Buffer.from(CLIENT_ID + ":" + CLIENT_SECRET).toString("base64"),
-    },
-    // data must be x-www-urlform-encoded, so it must be turned into a URL search param object
-    data: new URLSearchParams({
-      grant_type: "refresh_token",
-      refresh_token: refresh_token,
-    }),
-  })
-    .then((results) => {
-      // Successful case
-      console.log(results.data);
-      access_token = results.data.access_token;
-      refresh_token = results.data.refresh_token;
-      res.send(results.data);
+    axios({
+        url: refreshUrl,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            // From Spotify documentation
+            'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+        },
+        // data must be x-www-urlform-encoded, so it must be turned into a URL search param object
+        data: new URLSearchParams( {
+            "grant_type": "refresh_token",
+            "refresh_token": refresh_token
+        }),
     })
-    .catch((err) => {
-      // Handle errors
-      console.log(err);
-      res.send("Error. Check console log");
+    .then(results => {
+        // Successful case
+        console.log(results.data);
+        access_token = results.data.access_token;
+        refresh_token = results.data.refresh_token;
+        res.send(results.data);
+    })
+    .catch(err => {
+        // Handle errors
+        console.log(err);
+        res.send('Error. Check console log');
     });
 });
 
 // Route to log in to SPotify
 // Will likely be modified to fit into our own login endpoint
-app.get("/login", (req, res) => {
-  console.log("/login route");
+app.get('/login', (req, res) => {
+    console.log('/login route');
 
-  // should be a random number. For our purposes, this should be fine
-  var state = "superrandomnumber";
-  var scope = "user-read-private user-read-email";
+    // should be a random number. For our purposes, this should be fine
+    var state = "superrandomnumber";
+    var scope = 'user-read-private user-read-email';
 
-  params = {
-    response_type: "code",
-    client_id: CLIENT_ID,
-    scope: scope,
-    redirect_uri: REDIRECT_URI,
-    state: state,
-  };
+    params = {
+        response_type: 'code',
+        client_id: CLIENT_ID,
+        scope: scope,
+        redirect_uri: REDIRECT_URI,
+        state: state
+    };
 
-  // redirecting to spotify authorize endpoint to get a verification code
-  console.log(
-    "https://accounts.spotify.com/authorize?" +
-      new URLSearchParams(params).toString()
-  );
-  res.redirect(
-    "https://accounts.spotify.com/authorize?" +
-      new URLSearchParams(params).toString()
-  );
+    // redirecting to spotify authorize endpoint to get a verification code
+    console.log('https://accounts.spotify.com/authorize?' + new URLSearchParams(params).toString());
+    res.redirect('https://accounts.spotify.com/authorize?' + new URLSearchParams(params).toString());
+    
 });
 
-// Route to get current user's data. Verifies authentication works
-app.get("/me", (req, res) => {
-  console.log("/me route");
 
-  const meUrl = "https://api.spotify.com/v1/me";
+app.get('/search_song', async (req, res) =>{
+    console.log('/search route');
+    const q = req.query.q;
+    //limiting the number of results
+    limit = 10
 
-  axios({
-    url: meUrl,
-    method: "GET",
-    headers: {
-      Authorization: "Bearer " + access_token,
-    },
-  })
-    .then((results) => {
-      // Successful case
-      console.log(results.data);
-      res.send(results.data);
-    })
-    .catch((err) => {
-      // Handle errors
-      console.log(err);
-      res.send("Error. Check console log");
-    });
+    const options = {
+        method: 'GET',
+        url: 'https://shazam.p.rapidapi.com/search',
+        params: {term: q, locale: 'en-US', offset: '0', limit: limit},
+        headers: {
+        'X-RapidAPI-Key': '5c60a9f7e5msh6e3e990c63159adp184964jsn0ca34bbc7771',
+        'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+        }
+    };
+
+    axios.request(options).then(function (response) {
+        console.log(response.data);
+        //Checking to make sure there are results being sent backg an object to send back to client
+        params = {
+            tracks: []
+        };
+        // Iterating through each song and adding it to our respons JSON
+        for (let i = 0; i < limit; i++){
+            const title = response.data.tracks.hits[0].track.title
+            const key = response.data.tracks.hits[0].track.key
+            const imageLink = response.data.tracks.hits[0].track.images.coverart
+            const artist = response.data.tracks.hits[0].track.subtitle
+            params['tracks'].push({'title': title, 'key': key, 'artist': artist, 'imageLink': imageLink});
+        }
+        console.log(params);
+        res.send(params);
+    }).catch(function (error) {
+        console.error(error);
+        res.send(error.message);
+    }); 
+
 });
 
-// Route to search for items in Spotify (search for songs, playlists, etc)
-// Expects a string searchQuery for the string to serach and type, a list of types of results
-app.get("/search", async (req, res) => {
-  console.log("/search route");
-  const q = req.query.q;
-  const type = req.query.type;
 
-  const searchUrl =
-    "https://api.spotify.com/v1/search?q=" + q + "&type=" + type;
-  console.log("Search URL: ", searchUrl);
-
-  await axios({
-    url: searchUrl,
-    method: "GET",
-    headers: {
-      // 'Content-Type': 'application/json',
-      Authorization: "Bearer " + access_token,
-    },
-  })
-    .then((results) => {
-      // Successful case
-      console.log(results.data);
-
-      res.send(results.data.artists.items[0]);
-    })
-    .catch((err) => {
-      // Handle errors
-      console.log(err);
-      res.send("Error. Check console log");
-    });
-});
-
-// 9
+// 9 
 // Authentication Middleware.
 // const auth = (req, res, next) => {
 //     console.log(req.session);
@@ -232,9 +212,12 @@ app.get("/search", async (req, res) => {
 //     }
 //     next();
 //   };
-
+  
 // Authentication Required
 // app.use(auth);
 
+
+
+
 app.listen(3000);
-console.log("Server is listening on port 3000");
+console.log('Server is listening on port 3000');
