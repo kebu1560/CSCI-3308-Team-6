4894c29ff65e21e642ce6400baf91df9ce010428
Blake Hamilton <blha2078@colorado.edu>
2022-11-10 17:15:28 -0700

    Adding default user for testing purposes

--- init_data/insert_data.sql
+++ init_data/insert_data.sql
@@ -1,1 +1,2 @@
 INSERT INTO songs (song_id, title, image_link, artist) VALUES (123, 'Yellow Submarine', 'www.image.com', 'The Beatles');
+INSERT INTO users (username, password) VALUES ('Blake', 'pw');
