fa309d59ecb93814eaaa8f24eea2f081dd4d5e17
Blake Hamilton <blha2078@colorado.edu>
2022-11-13 13:34:06 -0700

    Adding FK university_id to users
    User must select their university when they register

--- init_data/create.sql
+++ init_data/create.sql
@@ -7,7 +7,11 @@ CREATE TABLE songs (
 
 CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY NOT NULL, 
-    password CHAR(50) NOT NULL
+    password CHAR(50) NOT NULL,
+    university_id INT,
+    CONSTRAINT fk_university_id
+        FOREIGN KEY (university_id) 
+            REFERENCES universities(university_id),
 );
 
 CREATE TABLE transactions (
